188920bf0d542170b3e1a235ae3aaa2f78626e1e
Cherish <785427346@qq.com>
2017-05-03 20:07:17 +0800

    矩阵连乘算法

--- src/test/java/cn/cherish/mboot/arithmetic/MatrixChainMultiplication.java
+++ src/test/java/cn/cherish/mboot/arithmetic/MatrixChainMultiplication.java
@@ -13,8 +13,8 @@ public class MatrixChainMultiplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int[] p = new int[n + 1];
+        int n = scanner.nextInt();// n 个 矩阵
+        int[] p = new int[n + 1];// n+1 个数值
         for (int i = 0; i <= n; i++) {
             p[i] = scanner.nextInt();
         }
